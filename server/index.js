3ada3340b23583e4c10ab6baa553cdf6182bfe48
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-08-07 00:36:08 +0100

    Implemented loan request API endpoint
    This commit introduces a new API endpoint (/api/request_for_loan) responsible for handling loan requests. The endpoint performs data validation and formatting to ensure accurate and consistent data storage. The full_name and email fields are trimmed and converted to lowercase, and the full_name is capitalized with proper formatting. The loan information, including loan_amount, repayment_duration, and a randomly generated transaction_id, is stored in the database using the Mongoose model. The loan_amount is also formatted as currency for user-friendly display. Success responses include a formatted success message with the loan amount and the associated transaction_id. In case of an error, appropriate error messages are provided.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Loan = require('./models/Loan');
+const generateRandomID = require('./models/GenerateRandomID');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
@@ -10,13 +12,13 @@ const port = 3000;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
+    console.error('Error connecting to MongoDB:', error);
   });
 
 // Routes goes here
@@ -24,12 +26,55 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    credentials: true,
+    credentials: true
   })
 );
 
-app.use("/", (req, res) => {
-    res.json({ connection: "Okay" });
+app.get('/', (req, res) => {
+  res.json({ connection: 'Okay' });
 });
 
+app.post('/api/request_for_loan', async (req, res) => {
+  // Connect to MongoDB
+  mongoose.connect(process.env.MONGO_URL);
+  try {
+    let { full_name, email, loan_amount, repayment_duration } = req.body;
+    // Trim and setting to lowercase
+    email = email.trim().toLowerCase();
+    //  capitalize the first letters of each word in a userName
+    full_name = full_name
+      .toLowerCase()
+      .trim()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+
+    // Formatting as money with the currency symbol and commas for thousands
+    const formattedAmount = Number(loan_amount).toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+
+    const newLoan = new Loan({
+      full_name,
+      email,
+      loan_amount: formattedAmount,
+      repayment_duration,
+      transaction_id: generateRandomID()
+    });
+
+    const savedLoan = await newLoan.save();
+
+    res.status(200).json({
+      message: `Loan request of ${formattedAmount} was successfully sent.`,
+      transaction_id: savedLoan.transaction_id
+    });
+  } catch (error) {
+    console.error('An error occur while requesting for loan:', error);
+    res.status(500).json({
+      message:
+        'An error occur while requesting for loan\nPlease try again later'
+    });
+  }
+});
 app.listen(port, () => console.log(`App is listening on port ${port}!`));
