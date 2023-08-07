55ec169266790000b17f9f6137de688978536c5b
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-08-07 03:05:23 +0100

    Added a endpoint to retrieve all loan requests
    Implemented a new API endpoint /api/get_all_loan_request that connects to MongoDB and retrieves all loan requests using the Mongoose Loan model. If successful, it responds with a JSON array containing the retrieved data. In case of an error, it returns an appropriate error message along with a 500 status code.
    
    This endpoint allows the client to fetch and display all loan requests for further processing and analysis.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Loan = require('./models/Loan');
-const generateRandomID = require('./models/GenerateRandomID');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Loan = require("./models/Loan");
+const generateRandomID = require("./models/GenerateRandomID");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
@@ -12,13 +12,13 @@ const port = 3000;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
 
 // Routes goes here
@@ -26,15 +26,15 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
+    credentials: true,
   })
 );
 
-app.get('/', (req, res) => {
-  res.json({ connection: 'Okay' });
+app.get("/", (req, res) => {
+  res.json({ connection: "Okay" });
 });
 
-app.post('/api/request_for_loan', async (req, res) => {
+app.post("/api/request_for_loan", async (req, res) => {
   // Connect to MongoDB
   mongoose.connect(process.env.MONGO_URL);
   try {
@@ -45,14 +45,14 @@ app.post('/api/request_for_loan', async (req, res) => {
     full_name = full_name
       .toLowerCase()
       .trim()
-      .split(' ')
+      .split(" ")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
+      .join(" ");
 
     // Formatting as money with the currency symbol and commas for thousands
-    const formattedAmount = Number(loan_amount).toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD'
+    const formattedAmount = Number(loan_amount).toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
     });
 
     const newLoan = new Loan({
@@ -60,21 +60,35 @@ app.post('/api/request_for_loan', async (req, res) => {
       email,
       loan_amount: formattedAmount,
       repayment_duration,
-      transaction_id: generateRandomID()
+      transaction_id: generateRandomID(),
     });
 
     const savedLoan = await newLoan.save();
 
     res.status(200).json({
       message: `Loan request of ${formattedAmount} was successfully sent.`,
-      transaction_id: savedLoan.transaction_id
+      transaction_id: savedLoan.transaction_id,
     });
   } catch (error) {
-    console.error('An error occur while requesting for loan:', error);
+    console.error("An error occur while requesting for loan:", error);
     res.status(500).json({
       message:
-        'An error occur while requesting for loan\nPlease try again later'
+        "An error occur while requesting for loan, Please try again later",
     });
   }
 });
+
+app.get("/api/get_all_loan_request", async (req, res) => {
+  // Connect to MongoDB
+  mongoose.connect(process.env.MONGO_URL);
+  try {
+    res.json(await Loan.find());
+  } catch (error) {
+    console.error("Unable to get request:", error);
+    res.status(500).json({
+      message: "Unable to get request, Please try again later",
+    });
+  }
+});
+
 app.listen(port, () => console.log(`App is listening on port ${port}!`));
