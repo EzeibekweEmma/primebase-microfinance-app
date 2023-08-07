40b1493a524382b8ba7e2d347542ea8511d2615f
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-08-07 21:26:08 +0100

    modify length of ID to be 15 characters long

--- server/models/GenerateRandomID.js
+++ server/models/GenerateRandomID.js
@@ -1,9 +1,9 @@
 function generateRandomID() {
   // Function to generate a random ID
-  const length = 13; // Length of the generated ID
+  const IDLength = 15; // Length of the generated ID
   const characters = "0123456789";
   let ID = "";
-  for (let i of characters) {
+  for (let i = 0; i < IDLength; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     ID += characters.charAt(randomIndex);
   }
