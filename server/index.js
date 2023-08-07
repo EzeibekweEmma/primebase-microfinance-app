195fc60f6775566b279252894bcb9dfcb56dc0ad
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-08-07 21:35:52 +0100

    delete unused code

--- server/index.js
+++ server/index.js
@@ -8,19 +8,6 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
-
 // Routes goes here
 app.use(express.json());
 app.use(
